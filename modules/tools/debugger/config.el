8acd54a949349ac4c2dc602bd7d227ef80442ff2
Thierry Ghélew <tlux@ghelew.net>
2025-01-27 19:15:53 +0100

    feat(debugger): Add support for Zig
    Include Zig using cc format.

--- modules/tools/debugger/config.el
+++ modules/tools/debugger/config.el
@@ -1,7 +1,9 @@
 ;;; tools/debugger/config.el -*- lexical-binding: t; -*-
 
 (defvar +debugger--dap-alist
-  `(((:lang cc +lsp)         :after ccls        :require (dap-lldb dap-gdb-lldb))
+  `(
+    ((:lang zig +lsp)        :after zls         :require (dap-lldb dap-gdb-lldb))
+    ((:lang cc +lsp)         :after ccls        :require (dap-lldb dap-gdb-lldb))
     ((:lang elixir +lsp)     :after elixir-mode :require dap-elixir)
     ((:lang go +lsp)         :after go-mode     :require dap-dlv-go)
     ((:lang gdscript +lsp)   :after gdscript-mode :require dap-gdscript)
